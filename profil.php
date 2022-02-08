261201b86b12030a6a94871d3d10b5c2003d0a6b
adriennnnn <95212982+adriennnnn@users.noreply.github.com>
2022-02-08 09:54:52 +0100

    ajout de dossier + img bg fin css profile

--- profil.php
+++ profil.php
@@ -8,10 +8,11 @@ $result = $pdo->fetch(PDO::FETCH_ASSOC);
 
 ?>
 
-<?php include 'partials/header.php'; 
+<?php
+//  include 'partials/header.php'; 
 ?>
 
-<link href="style_profil.css" rel="stylesheet">
+<link href="./style/style_profil.css" rel="stylesheet">
 <div class="profile-section">
     <div class="profile">
         <div class="profile-image">
@@ -19,14 +20,16 @@ $result = $pdo->fetch(PDO::FETCH_ASSOC);
         </div>
         <div class="profile-user-settings">
             <h1 class="profile-user-name"><?= $result['pseudo'] ?></h1>
+            <form action="./profil_edit.php">
             <button class="btn profile-edit-btn">Edit Profile</button>
             <button class="btn profile-settings-btn" aria-label="profile settings"><i class="fas fa-cog" aria-hidden="true"></i></button>
+            </form>
         </div>
         <div class="profile-stats">
             <ul>
-                <li><span class="profile-stat-count">164</span> posts</li>
-                <li><span class="profile-stat-count">188</span> followers</li>
-                <li><span class="profile-stat-count">206</span> following</li>
+                <li><span class="profile-stat-count">2727</span> posts</li>
+                <li><span class="profile-stat-count">42</span> followers</li>
+                <li><span class="profile-stat-count">rien</span> following</li>
             </ul>
         </div>
         <div class="profile-bio">
@@ -42,11 +45,11 @@ $result = $pdo->fetch(PDO::FETCH_ASSOC);
 <div class="profile-section">
     <div class="gallery">
         <div class="gallery-item" tabindex="0">
-            <img src="https://images.unsplash.com/photo-1511765224389-37f0e77cf0eb?w=500&h=500&fit=crop" class="gallery-image" alt="">
+            <img src="<!-- la il va y avoir un problem-->" class="gallery-image" alt="">
             <div class="gallery-item-info">
                 <ul>
-                    <li class="gallery-item-likes"><span class="visually-hidden">Likes:</span><i class="fas fa-heart" aria-hidden="true"></i> 56</li>
-                    <li class="gallery-item-comments"><span class="visually-hidden">Comments:</span><i class="fas fa-comment" aria-hidden="true"></i> 2</li>
+                    <li class="gallery-item-likes"><span class="visually-hidden">Likes:</span><i class="fas fa-heart" aria-hidden="true"></i> <!-- nombre de like--></li>
+                    <li class="gallery-item-comments"><span class="visually-hidden">Comments:</span><i class="fas fa-comment" aria-hidden="true"></i> <!-- nombre de commentaire --></li>
                 </ul>
             </div>
         </div>
