d8a2baaa1afa54ef6e3a2ed03f8879fcf338b684
adriennnnn <95212982+adriennnnn@users.noreply.github.com>
2022-05-09 16:55:26 +0200

    env

--- utils/link.php
+++ utils/link.php
@@ -1,7 +1,7 @@
 <?php
 try
     {
-        $bdd = new PDO('mysql:host=141.94.22.233;dbname=portfolio_adrien', 'adrien', 'garage404', [
+        $bdd = new PDO('mysql:host=141.94.22.233;dbname=portfolio_adrien', getenv('USERNAME'), getenv('PASSWORD'), [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
     }
